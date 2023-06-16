b1d209add7cba31bcba0a29e7c132bf9c82573a9
DmGsOuNaK <sounak.swamiji@gmail.com>
2023-06-16 13:12:37 +0530

    Updates

---
+++ precache-manifest.622d0f01afe5a08472ac493d9921137b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a8415ea6bcb78784df6",
+    "url": "/Calculator_reactJS/static/css/main.6b1b5f01.chunk.css"
+  },
+  {
+    "revision": "6a8415ea6bcb78784df6",
+    "url": "/Calculator_reactJS/static/js/main.6a8415ea.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/Calculator_reactJS/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/Calculator_reactJS/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "72c7d3fa1f7ea0178185",
+    "url": "/Calculator_reactJS/static/js/runtime~main.72c7d3fa.js"
+  },
+  {
+    "revision": "1ae547ebdf26cfe7adc4968ceac74980",
+    "url": "/Calculator_reactJS/index.html"
+  }
+];
